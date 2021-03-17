9f1c04728a029a930da2e3d973e5300125d0ff68
karthikeyansa <karthikeyansa39@gmail.com>
2021-03-17 23:17:48 +0530

    removed files

--- src/poll/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('',views.home,name='home'),
-    path('create/',views.create,name='create'),
-    path('vote/<int:id>/',views.vote,name='vote'),
-    path('view/<int:id>/',views.view,name = 'view'),
-    path('delete/<int:id>',views.delete,name = 'delete'),
-]
